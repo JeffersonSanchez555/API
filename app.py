7c4c2502c9ec820ba31415af84629452bd5a3910
Triplez <sanchezjefferson555@gmail.com>
2026-09-21 12:29:01 -0500

    Conexion api a react

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from database import conectar_bd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/probar")
@@ -42,9 +44,7 @@ def obtener_hojasvida():
 
     return hojas_vida
 
-# ---------------------------------------------------------
-# HOJAS DE VIDA (CRUD)
-# ---------------------------------------------------------
+
 
 @app.route("/api/registrohv", methods=["POST"])
 def registrohvida():
